45ff32e2910766ef3d8f76a6701257f8bcec5a0f
Rafal Hes <rafal.hes@yahoo.com>
2023-08-08 17:26:44 +0200

    feat: add AuthProviders component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,17 @@
 import { NavLinks } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
+import AuthProviders from './AuthProviders';
 
 const Navbar = () => {
-  return <nav className='flexBetween navbar'>
-      <div className='flex-1 flexStart gap-10'>
-        <Link href='/'>
-          <Image src='/logo.svg' alt='Flexible' width={115} height={42} />
-
-          </Link>
-        <ul className='xl:flex hidden text-spall gap-7'>
+  const session = {};
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" alt="Flexible" width={115} height={42} />
+        </Link>
+        <ul className="xl:flex hidden text-spall gap-7">
           {NavLinks.map((link) => (
             <Link href={link.href} key={link.key}>
               {link.text}
@@ -17,7 +19,18 @@ const Navbar = () => {
           ))}
         </ul>
       </div>
-    </nav>;
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">Share your work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
