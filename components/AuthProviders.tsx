badc45c68d6f372ececb017dc20669a8070a9171
Rafal Hes <rafal.hes@yahoo.com>
2023-08-09 17:47:41 +0200

    feat: add signIn function to AuthProvider

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -19,21 +19,21 @@ const AuthProviders = () => {
   useEffect(() => {
     const fetchProviders = async () => {
       const res = await getProviders();
-      console.log(res);
       setProviders(res);
-    }
+    };
     fetchProviders();
   }, []);
-  
+
   if (providers) {
     return (
       <div>
         {Object.values(providers).map((provider: Provider, i) => (
-          <button key={i}>{provider.id}</button>
+          <button key={i} onClick={() => signIn(provider?.id)}>
+            {provider.id}
+          </button>
         ))}
-          
       </div>
-    )
+    );
   }
 };
 
