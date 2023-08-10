c62f7c5ad55897cdfcd6181977499d9642d19a8b
Rafal Hes <rafal.hes@yahoo.com>
2023-08-10 20:58:43 +0200

    feat: add session nad signIn functions

--- lib/session.ts
+++ lib/session.ts
@@ -4,7 +4,8 @@ import { AdapterUser } from 'next-auth/adapters';
 import GoogleProvider from 'next-auth/providers/google';
 import { jsonwebtoken } from 'jsonwebtoken';
 import { JWT } from 'next-auth/jwt';
-import { SessionInterface } from '@/common.types';
+import { SessionInterface, UserProfile } from '@/common.types';
+import { createUser, getUser } from './actions';
 
 export const authOptions = {
   providers: [
@@ -23,13 +24,32 @@ export const authOptions = {
   },
   callbacks: {
     async session({ session }) {
-      return session;
+      const email = session?.user?.email as string;
+
+      try {
+        const data = (await getUser(email)) as { user?: UserProfile };
+
+        const newSession = {
+          ...session,
+          user: {
+            ...session.user,
+            ...data?.user
+          }
+        };
+        return newSession;
+      } catch (error: any) {
+        console.log('Error retrieving user data ', error);
+        return session;
+      }
     },
+
     async signIn({ user }: { user: AdapterUser | User }) {
       try {
-        // get the user if it exists
+        const userExists = (await getUser(user?.email as string)) as { user: UserProfile };
 
-        // if it doesn't exist, create it
+        if (!userExists.user) {
+          await createUser(user.name as string, user.email as string, user.image as string);
+        }
 
         return true;
       } catch (error: any) {
@@ -41,7 +61,7 @@ export const authOptions = {
 };
 
 export async function getCurrentUser() {
-  const session = await getServerSession(authOptions) as SessionInterface;
+  const session = (await getServerSession(authOptions)) as SessionInterface;
 
   return session;
 }
