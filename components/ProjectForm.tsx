03687dfd88f1510625189961b2b5e6bbce1f6270
Rafal Hes <rafal.hes@yahoo.com>
2023-08-13 15:49:02 +0200

    feat: add FormField component

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+'use client';
 
-const ProjectForm = () => {
+import { SessionInterface } from '@/common.types';
+import Image from 'next/image';
+import React from 'react';
+
+type Props = {
+  type: string;
+  session: SessionInterface;
+};
+
+const ProjectForm = ({ type, session }: Props) => {
+  const handleChangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const handleSubmit = (e: React.FormEvent) => {};
+
+  const form = {
+    image: '',
+    title: ''
+  };
+
+  const handleStateChange = (fieldName: string, value: string) => {};
+
   return (
-    <div>ProjectForm</div>
-  )
-}
+    <form onSubmit={handleSubmit} className="flexStart form">
+      <div className="flexStart form_image-container">
+        <label htmlFor="poster" className="flexCenter form_image-label">
+          {!form.image && 'Choose a poster for your project'}
+        </label>
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          required={type === 'create'}
+          className="form_image-input"
+          onChange={handleChangeImage}
+        />
+        {form.image && (
+          <Image
+            src={form.image}
+            className="sm:p-10 object-contain z-20"
+            alt="project poster"
+            fill
+          />
+        )}
+      </div>
 
-export default ProjectForm
+      <FormField
+        title="Title"
+        state={form.title}
+        placeholder="Flexibble"
+        setState={(value) => handleStateChange('title', value)}
+      />
+    </form>
+  );
+};
+
+export default ProjectForm;
