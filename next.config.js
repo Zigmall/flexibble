f6289460c56887c61569390c486e9b28758f5d54
Rafal Hes <rafal.hes@yahoo.com>
2023-08-14 20:03:16 +0200

    feat: add cloudinary to domain list in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {
-    domains: ['lh3.googleusercontent.com'],
-}}
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com']
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
